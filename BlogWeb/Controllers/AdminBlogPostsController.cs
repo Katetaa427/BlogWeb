30d8ed79ea8d754a4ceb031991683581763f68c1
kate <katetaa427@gmail.com>
2023-09-25 10:42:31 -0700

    AdminTags

---
+++ BlogWeb/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,75 @@
+﻿using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository tagRepository;
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository, IBlogPostRepository blogPostRepository)
+        {
+            this.tagRepository = tagRepository;
+            this.blogPostRepository = blogPostRepository;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            //get tags from repository
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            ///map view model to domain model 
+            var blogPost = new BlogPost
+            {
+                Heading = addBlogPostRequest.Heading,
+                PageTitle = addBlogPostRequest.PageTitle,
+                Content = addBlogPostRequest.Content,
+                ShortDescription = addBlogPostRequest.ShortDescription,
+                FeaturedImageUrl = addBlogPostRequest.FeaturedImageUrl,
+                UrlHandle = addBlogPostRequest.UrlHandle,
+                PublishedDate = addBlogPostRequest.PublishedDate,
+                Author = addBlogPostRequest.Author,
+                Visible = addBlogPostRequest.Visible,
+
+            };
+
+            //map tags from selected tags 
+            var selectedTags = new List<Tag>();
+
+            foreach (var selectedTagId in addBlogPostRequest.SelectedTags) 
+            { 
+                var selectedTagIdAsGuid = Guid.Parse(selectedTagId);
+                var exisitingTag = await tagRepository.GetAsync(selectedTagIdAsGuid);
+
+                if (exisitingTag != null)
+                {
+                    selectedTags.Add(exisitingTag);
+                }
+            }
+            
+            //mapping tags back to domain model 
+            blogPost.Tags = selectedTags;
+
+            await blogPostRepository.AddAsync(blogPost);
+
+            return RedirectToAction("Add");
+        }
+    }
+}
--- BlogWeb/Models/ViewModels/AddBlogPostRequest.cs
+++ BlogWeb/Models/ViewModels/AddBlogPostRequest.cs
@@ -1,8 +1,9 @@
-﻿namespace Blog.Web.Models.Domain
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.Web.Models.ViewModels
 {
-    public class BlogPost
+    public class AddBlogPostRequest
     {
-        public Guid Id { get; set; }
         public string Heading { get; set; }
         public string PageTitle { get; set; }
         public string Content { get; set; }
@@ -12,6 +13,11 @@
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
         public bool Visible { get; set; }
-        public ICollection<Tag> Tags { get; set; }
+
+        //display tags
+        public IEnumerable<SelectListItem> Tags { get; set; }
+
+        //collect tags
+        public string[] SelectedTags { get; set; } = Array.Empty<string>();
     }
 }
